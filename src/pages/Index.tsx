c8f5ae424073ae733adeedc236791c8387e33ff7
Marcello De Feo <marcellodefeo@outlook.com>
2024-12-25 15:19:14 -0500

    Adding templates, enhancing json data

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,7 @@
-import AboutPage from './AboutPage';
 import CVPage from './CVPage';
 import HomePage from './HomePage';
 import MusicPage from './MusicPage';
 import PortfolioPage from './PortfolioPage';
 
 
-export { AboutPage, CVPage, HomePage, MusicPage, PortfolioPage };
+export { CVPage, HomePage, MusicPage, PortfolioPage };
