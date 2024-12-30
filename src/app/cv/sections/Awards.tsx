fbf9771a360402a93b1b984918c07399fc640f40
Marcello De Feo <marcellodefeo@outlook.com>
2024-12-30 00:18:45 -0500

    Minor style changes to CV

--- src/app/cv/sections/Awards.tsx
+++ src/app/cv/sections/Awards.tsx
@@ -13,43 +13,43 @@ export default function AchievementsAndAwards() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           <div className="card bg-white p-6 shadow-lg rounded-xl">
             <FontAwesomeIcon icon={faTrophy} className="text-yellow-500 text-3xl mb-4" />
-            <h3 className="font-semibold text-lg">Best of Award for Public Service</h3>
-            <p className="text-gray-700">
+            <h3 className="font-semibold text-md leading-5">Best of Award for Public Service</h3>
+            <p className="text-sm">
               Philadelphia Holocaust Remembrance Foundation Website and Interactive Timeline.
             </p>
           </div>
           <div className="card bg-white p-6 shadow-lg rounded-xl">
             <FontAwesomeIcon icon={faTrophy} className="text-yellow-500 text-3xl mb-4" />
-            <h3 className="font-semibold text-lg">Gold Award for Broadcast/Electronic Digital</h3>
-            <p className="text-gray-700">
+            <h3 className="font-semibold text-md leading-5">Gold Award for Broadcast/Electronic Digital</h3>
+            <p className="text-sm">
               Public Service award for the Philadelphia Holocaust Remembrance Foundation Website.
             </p>
           </div>
           <div className="card bg-white p-6 shadow-lg rounded-xl">
             <FontAwesomeIcon icon={faTrophy} className="text-yellow-500 text-3xl mb-4" />
-            <h3 className="font-semibold text-lg">Gold Award for Product Microsites</h3>
-            <p className="text-gray-700">
+            <h3 className="font-semibold text-md leading-5">Gold Award for Product Microsites</h3>
+            <p className="text-sm">
               Digital Advertising award for the IKEA New Movers Microsite.
             </p>
           </div>
           <div className="card bg-white p-6 shadow-lg rounded-xl">
             <FontAwesomeIcon icon={faTrophy} className="text-yellow-500 text-3xl mb-4" />
-            <h3 className="font-semibold text-lg">Gold Award for Consumer Websites</h3>
-            <p className="text-gray-700">
+            <h3 className="font-semibold text-md leading-5">Gold Award for Consumer Websites</h3>
+            <p className="text-sm">
               Digital Advertising award for the IKEA Interactive Seasonal Catalog.
             </p>
           </div>
           <div className="card bg-white p-6 shadow-lg rounded-xl">
             <FontAwesomeIcon icon={faTrophy} className="text-yellow-500 text-3xl mb-4" />
-            <h3 className="font-semibold text-lg">Web Project of the Year Nominee</h3>
-            <p className="text-gray-700">
+            <h3 className="font-semibold text-md leading-5">Web Project of the Year Nominee</h3>
+            <p className="text-sm">
               Philadelphia Holocaust Remembrance Foundation website development.
             </p>
           </div>
           <div className="card bg-white p-6 shadow-lg rounded-xl">
             <FontAwesomeIcon icon={faTrophy} className="text-yellow-500 text-3xl mb-4" />
-            <h3 className="font-semibold text-lg">Webby Awards Honoree</h3>
-            <p className="text-gray-700">
+            <h3 className="font-semibold text-md leading-5">Webby Awards Honoree</h3>
+            <p className="text-sm">
               IKEA Seasonal Launch Vehicle digital catalog.
             </p>
           </div>
