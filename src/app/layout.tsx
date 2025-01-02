1852cca249421443b8b8fdd6b96408773a8ba21c
Marcello De Feo <marcellodefeo@outlook.com>
2025-01-02 18:09:54 -0500

    Accessibility, high contrast, theme switching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import UISettings from "@/components/ui/UISettings";
 import "../styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "../styles/accessibility.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" data-theme="nord">
+      <body>
+        <UISettings />
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
