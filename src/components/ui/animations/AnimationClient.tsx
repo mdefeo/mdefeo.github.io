137cf26b66eb7bb077accbbc05bdac85dc788d15
Marcello De Feo <marcellodefeo@outlook.com>
2025-01-05 00:47:42 -0500

    Initial commit

--- src/components/ui/animations/AnimationClient.tsx
+++ src/components/ui/animations/AnimationClient.tsx
@@ -3,26 +3,24 @@
 import React, { useEffect, useState, useRef } from "react";
 import Signature from "./Signature";
 import { animateBorders } from "@/animations/borderAnimation";
-import ViewportBorders from "@/components/ui/animations/ViewportBorders"; // Assuming you need this as well
+import ViewportBorders from "@/components/ui/animations/ViewportBorders";
 
 const AnimationClient: React.FC = () => {
   const [showSignature, setShowSignature] = useState(false);
 
-  // Create refs for the Popover.Button and FontAwesomeIcon elements
   const popoverButtonRef = useRef<HTMLButtonElement | null>(null);
   const iconRef = useRef<SVGSVGElement | null>(null);
 
   useEffect(() => {
-    // Start the border animation and trigger signature after borders complete
     animateBorders(() => {
-      setShowSignature(true);  // Trigger the signature animation after borders are done
-    }, iconRef, popoverButtonRef); // Pass refs for opacity animation
+      setShowSignature(true); 
+    }, iconRef, popoverButtonRef);
   }, []);
 
   return (
     <>
       <ViewportBorders />
-      {showSignature && <Signature />}  {/* Show signature after the borders finish */}
+      {showSignature && <Signature />}
     </>
   );
 };
